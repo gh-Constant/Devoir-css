9160ea62c74be93cdd0ebffaf9604297910fe655
Constant <constantsuchet@gmail.com>
2024-09-24 11:01:14 +0200

    oui

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Creation de la liste des jeux à mettre dans le site web
 
 const games = [
-    { title: "League of Legends", developer: "Riot Games", image: "lol.jpg" },
-    { title: "Minecraft", developer: "Mojang Studios", image: "minecraft.png" },
-    { title: "Mario Bros", developer: "Nintendo", image: "supermariobros.jpg" },
-    { title: "GTA 5", developer: "Rockstar Games", image: "gta.jpg" },
-    { title: "FIFA 23", developer: "Electronic Arts", image: "fifa.jpg" },
-    { title: "Tetris", developer: "Aleksei Pajitnov", image: "tetris.jpeg" },
-    { title: "Pokemon Lune", developer: "Nintendo", image: "pokemon.jpg" }
+    { title: "League of Legends", developer: "Riot Games", image: "public/images/lol.jpg" },
+    { title: "Minecraft", developer: "Mojang Studios", image: "public/images/minecraft.png" },
+    { title: "Mario Bros", developer: "Nintendo", image: "public/images/supermariobros.jpg" },
+    { title: "GTA 5", developer: "Rockstar Games", image: "public/images/gta.jpg" },
+    { title: "FIFA 23", developer: "Electronic Arts", image: "public/images/fifa.jpg" },
+    { title: "Tetris", developer: "Aleksei Pajitnov", image: "public/images/tetris.jpeg" },
+    { title: "Pokemon Lune", developer: "Nintendo", image: "public/images/pokemon.jpg" }
 ];
 
 // Fonction principale pour créer les jeux sur l'interface
@@ -42,9 +42,9 @@ function createGameCard(game) {
 
 // La fonction sera éxecuté directement aprés le chargement du site (DOMContentLoaded) et rajoutera dans la div gameGrid les jeux un à un
 
-function renderGameGrid() {
+function createSite() {
     const gameGrid = document.getElementById('gameGrid');
     gameGrid.innerHTML = games.map(createGameCard).join('');
 }
 
-document.addEventListener('DOMContentLoaded', renderGameGrid);
+document.addEventListener('DOMContentLoaded', createSite);
